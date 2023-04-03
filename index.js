85c8215af2c6e6a7fc2ce4884b504f326e6e7dc5
Mariia Rozbitska <melissa180lily@gmail.com>
2023-04-03 19:48:26 +0300

    feat: add changes to project

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-function ready() {
-
 const menu = document.querySelector('.menu__body');
 const menuBtn = document.querySelector('.menu__icon');
-
+const menuLink = document.querySelectorAll('.menu__list-link');
 const body = document.body;
 
-if (menu && menuBtn) {
-	menuBtn.addEventListener('click', () => {
+function ready() {
+	//if (menu && menuBtn) {
+	menuBtn.addEventListener('click', function () {
 		menu.classList.toggle('active');
 		menuBtn.classList.toggle('active');
 		body.classList.toggle('lock');
-	})
-
-	menu.querySelectorAll('.menu__list-link').forEach(link => {
-		link.addEventListener('click', () => {
-			menu.classList.remove('active');
-			menuBtn.classList.remove('active');
-			body.classList.remove('lock');
-		});
 	});
-}
-}
+
+	menu.addEventListener('click', function (event) {
+		event.target.closest('.menu__body').classList.remove('active');
+		body.classList.remove('lock');
+		// console.log(event.target);
+	});	
+};
+
+
+	//menuLink.forEach(link => {
+		//link.addEventListener('click', () => {
+			//menu.classList.remove('active');
+			//menuBtn.classList.remove('active');
+			//body.classList.remove('lock');
+		//});
+	//});
+ //}
+
+
 
 document.addEventListener('DOMContentLoaded', ready);
